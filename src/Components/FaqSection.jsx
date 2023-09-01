6914bcda4dba913481b5aac5dab0e47faf1a3a80
thomasAmara <thomastarawali@gmail.com>
2023-09-01 16:05:35 +1000

    Faq section

---
+++ src/Components/FaqSection.jsx
@@ -0,0 +1,21 @@
+import { Box, Text } from '@chakra-ui/react';
+import React from 'react';
+
+export default function FaqSection() {
+  return (
+    <div>
+      FAQ
+      <Box>
+        <Box>
+          <Text>Frequently Ask</Text>
+          <Text>Question</Text>
+        </Box>
+
+        <Box>
+          <Box></Box>
+          <Box></Box>
+        </Box>
+      </Box>
+    </div>
+  );
+}
