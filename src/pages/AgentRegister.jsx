10db0835c211898c3ace04fa85e0f42069be55b2
thomasAmara <thomastarawali@gmail.com>
2023-09-11 21:25:51 +1000

    refactor page

--- src/pages/AgentRegister.jsx
+++ src/pages/AgentRegister.jsx
@@ -11,6 +11,8 @@ import React, { useState } from 'react';
 import Bg from '../Assets/register.png';
 import { Link } from 'react-router-dom';
 import { useMediaQuery } from '@chakra-ui/react';
+import IndividualAgent from '../Components/IndividualAgent';
+import Proprietor from '../Components/Proprietor';
 
 export default function AgentRegister() {
   const [activePage, setActivePage] = useState('page1');
@@ -18,7 +20,6 @@ export default function AgentRegister() {
   const [isLargerThan1280] = useMediaQuery('(min-width: 1280px)');
 
   const handlePage = () => {
-    console.log('page 1');
     setActivePage('page1');
   };
 
@@ -132,254 +133,12 @@ export default function AgentRegister() {
                 </Text>
               </Box>
             </Box>
-            <Box
-              mt='15px'
-              width='100%'
-              display='flex'
-              flexDirection='column'
-              justifyContent='space-between'
-            >
-              <Text textAlign='start' color='#545454' fontSize='18px'>
-                Personal Details
-              </Text>
-
-              <SimpleGrid
-                column={2}
-                minChildWidth='120px'
-                width='100%'
-                spacing='15px'
-              >
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>First Name</Text>
-                    <Input name='firstName' />
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Middle Name</Text>
-                    <Input name='middleName' />
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-              <SimpleGrid minChildWidth='148px' width='100%' spacing='15px'>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Last Name</Text>
-                    <Input name='lastName' />
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Date of Birth</Text>
-                    <Input name='DOB' />
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-              <SimpleGrid minChildWidth='148px' width='100%' spacing='15px'>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Phone Number</Text>
-                    <Input name='phoneNumber' />
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Email Address</Text>
-                    <Input name='email' />
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-              <SimpleGrid
-                mt='15px'
-                minChildWidth='148px'
-                width='100%'
-                spacing='15px'
-              >
-                <Box>
-                  <Wrap>
-                    {/* <Text>Phone Number</Text> */}
-
-                    <Select>
-                      <option>Residential</option>
-                    </Select>
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Select>
-                      <option>State</option>
-                    </Select>
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Select>
-                      <option>LGA</option>
-                    </Select>
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-              {/* <SimpleGrid>DOCUMENT</SimpleGrid> */}
-            </Box>
-            <Box mt='25px'>
-              <Text textAlign='start' color='#545454' fontSize='18px'>
-                Documents
-              </Text>
-              <SimpleGrid
-                column={2}
-                minChildWidth='120px'
-                width='100%'
-                spacing='15px'
-              >
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Passport Photo</Text>
-                    <Input name='firstName' />
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Prof of Address</Text>
-                    <Input name='lastName' />
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-              <SimpleGrid
-                mt='15px'
-                minChildWidth='148px'
-                width='100%'
-                spacing='15px'
-              >
-                <Box>
-                  <Wrap>
-                    {/* <Text>Phone Number</Text> */}
-
-                    <Select>
-                      <option>ID Type</option>
-                    </Select>
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Select>
-                      <option>ID Number</option>
-                    </Select>
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Select>
-                      <option>ID Document</option>
-                    </Select>
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-            </Box>
-            <Box mt='25px'>
-              <Text textAlign='start' color='#545454' fontSize='18px'>
-                Banking Details
-              </Text>
-              <SimpleGrid
-                column={2}
-                minChildWidth='120px'
-                width='100%'
-                spacing='15px'
-              >
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Bank Name</Text>
-                    <Input name='bankName' />
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Bank Account Number</Text>
-                    <Input name='bankAccountNumber' />
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-              <SimpleGrid
-                column={2}
-                minChildWidth='120px'
-                width='100%'
-                spacing='15px'
-              >
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>BVN</Text>
-                    <Input name='firstName' />
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>TAX ID</Text>
-                    <Input name='lastName' />
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-            </Box>
-            <Box mt='25px'>
-              <Text textAlign='start' color='#545454' fontSize='18px'>
-                Business
-              </Text>
-              <SimpleGrid
-                column={2}
-                minChildWidth='120px'
-                width='100%'
-                spacing='15px'
-              >
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Registered Business Name</Text>
-                    <Input name='Registered Business Name' />
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>Agent Code </Text>
-                    <Input name='agentCode' />
-                    <Text fontSize='10px' fontWeight='400' color='#942F8B'>
-                      This is the Bank Account from which you will always fund
-                      your Wallet/Virtual Account.
-                    </Text>
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-              <SimpleGrid
-                mt='15px'
-                minChildWidth='148px'
-                width='100%'
-                spacing='15px'
-              >
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>BVN</Text>
-                    <Input name='firstName' />
-                  </Wrap>
-                </Box>
-                <Box>
-                  <Wrap>
-                    <Text color='#942F8B'>TAX ID</Text>
-                    <Input name='lastName' />
-                  </Wrap>
-                </Box>
-              </SimpleGrid>
-              <SimpleGrid></SimpleGrid>
-            </Box>
-            <Box mt='15px'>
-              <Button
-                _hover={{ bg: '#C296BE' }}
-                bg='#862E7E'
-                maxWidth='600px'
-                height='70px'
-                width='100%'
-                p='10px'
-                borderRadius='10px'
-                color='#FFF'
-              >
-                Submit
-              </Button>
+            <Box className='tab'>
+              {activePage === 'page1' ? (
+                <Proprietor />
+              ) : activePage === 'page2' ? (
+                <IndividualAgent />
+              ) : null}
             </Box>
           </Box>
         </Box>
