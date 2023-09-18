aa49e5ef10a555c9639a11cd549e810cfebe1524
thomasAmara <thomastarawali@gmail.com>
2023-09-19 07:21:19 +1000

    Date validation

--- src/Components/DatePicker.jsx
+++ src/Components/DatePicker.jsx
@@ -3,20 +3,15 @@ import { useField, ErrorMessage } from 'formik';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { Box } from '@chakra-ui/react';
+import { moment } from 'moment';
 
 const DatePickerField = ({ ...props }) => {
-  //   let date = new Date();
-  //   let todayDate = moment(date).format('YYYY-MM-DD');
+  const [field, { setValue }] = useField(props);
 
-  //   console.log('new date', todayDate);
-  //   console.log('date', date);
-  // const [field, { setValue }] = useField(props);
-  const [field, , { form }] = useField(props);
-
-  const handleChange = (date) => {
-    form.setFieldValue(field.name, date);
-  };
-
+  // const handleChange = (date) => {
+  //   const formattedDate = date ? moment(date).format('YYYY-MM-DD') : ''; // Format the date
+  //   setValue(field.name, formattedDate);
+  // };
   return (
     <Box width='100%'>
       <DatePicker
@@ -25,19 +20,18 @@ const DatePickerField = ({ ...props }) => {
         // selected={field.value && new Date(field.value)}
         selected={field.value ? new Date(field.value) : null}
         // selected={moment(field.value).format('YYYY-MM-DD')}
-        // onChange={(val) => {
-        //   setValue(val);
-        // }}
-
-        onChange={handleChange}
+        onChange={(val) => {
+          setValue(val);
+        }}
+        // onChange={handleChange}
       />
-      <ErrorMessage component='div' name={field.name} style={errorMessage} />
+      {/* <ErrorMessage component='div' name={field.name} style={errorMessage} /> */}
     </Box>
   );
 };
 export default DatePickerField;
-const errorMessage = {
-  color: 'red',
-  position: 'absolute',
-  fontSize: '10px',
-};
+// const errorMessage = {
+//   color: 'red',
+//   position: 'absolute',
+//   fontSize: '10px',
+// };
